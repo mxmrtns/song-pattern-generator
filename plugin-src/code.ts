e4822fd797e39647e883d294a242a0eb7b71746b
Max Mertens <maxmertens@gmx.de>
2022-07-18 09:45:49 +0200

    setup

--- plugin-src/code.ts
+++ plugin-src/code.ts
@@ -1,20 +1,223 @@
-figma.showUI(__html__, { themeColors: true, height: 300 });
 
+figma.showUI(__html__, {width: 240, height: 450 });
+
+figma.ui.resize(240, 500);
+
+const padding = 4;
+const gap = padding / 2;
+
+const pillarW = 12;
+const pillarH = 24;
+
+let bubblePos = [];
+let vertexPos = [];
+
+
+let frame;
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
 figma.ui.onmessage = (msg) => {
-  if (msg.type === "create-rectangles") {
-    const nodes = [];
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+
+  if (msg.type != 'clone') {
+
+    vertexPos = [];
+
+    const colorValPillar = msg.colorSelectPillar.split(' ');
+    const colorValBubble = msg.colorSelectBubble.split(' ');
+    const colorValVector = msg.colorSelectVector.split(' ');
+
+    // Logic for clearing the old Frame ——————————————————————————————————————————————————————————————
+    const oldFrame = figma.currentPage.findOne(node => node.type === "FRAME" && node.name === "Song");
+
+    if (oldFrame !== null) {
+      oldFrame.remove();
+    } else {};
+
+
+    // Creating and setting the size of the frame ————————————————————————————————————————————————————
+    frame = figma.createFrame();
+
+    let canvasHeight = padding * 2 + msg.rows * pillarH + (msg.rows - 1) * gap;
+    let canvasWidth = padding * 2 + msg.cols * pillarW + (msg.cols - 1) * gap;
+
+    frame.x = 0
+    frame.y = 0
+    frame.name = "Song"
+    frame.constrainProportions = true;
+    frame.resize(canvasWidth, canvasHeight)
+
+
+    // Drawing the elements on the frame with loops ——————————————————————————————————————————————————
+    const nodes: SceneNode[] = [];
+
+    for (let i = 0; i < msg.rows; i++) {
+
+      bubblePos[i] = [];
+
+      for (let l = 0; l < msg.cols; l++) {
+
+
+        //Pillars ——————————————————————————————————————————————————
+        let calcX = l * (pillarW + gap) + padding;
+        let calcY = i * (pillarH + gap) + padding;
+
+        let calcRTop = ((0 + (i + l) * 100) % 200);
+        let calcRBot = ((100 + (i + l) * 100) % 200);
+
+        const pillar = figma.createRectangle();
+        pillar.resize(pillarW, pillarH);
+        pillar.topLeftRadius = calcRTop;
+        pillar.topRightRadius = calcRTop;
+        pillar.bottomLeftRadius = calcRBot;
+        pillar.bottomRightRadius = calcRBot;
+        pillar.x = calcX;
+        pillar.y = calcY;
+
+        pillar.fills = [{
+          type: 'SOLID',
+          color: {
+            r: Number(colorValPillar[0]),
+            g: Number(colorValPillar[1]),
+            b: Number(colorValPillar[2])
+          }
+        }];
+        frame.appendChild(pillar);
+        nodes.push(pillar);
+
+        //Bubbles ——————————————————————————————————————————————————
+
+        // X und Y positon der Bubbles berechnen
+        let calcBubbleX = l * (gap + pillarW) + padding;
+        let calcBubbleYTop = i * (gap + pillarH) + padding;
+        let calcBubbleYBot = i * (gap + pillarH) + padding + pillarH - pillarW;
+
+        //X und Y Pos in Object und Arry speichern
+        if ((l + i) % 2 == 0) {
+          // Bottom
+          bubblePos[i][l] = {
+            x: calcBubbleX,
+            y: calcBubbleYBot
+          };
+        } else {
+          // Top
+          bubblePos[i][l] = {
+            x: calcBubbleX,
+            y: calcBubbleYTop
+          };
+        }
+
+        if (Math.random() < 0.5) {
+          const bubble = figma.createEllipse();
+          bubble.x = bubblePos[i][l].x;
+          bubble.y = bubblePos[i][l].y;
+          bubble.resize(pillarW, pillarW)
+          bubble.fills = [{
+            type: 'SOLID',
+            color: {
+              r: Number(colorValBubble[0]),
+              g: Number(colorValBubble[1]),
+              b: Number(colorValBubble[2])
+            }
+          }];
+
+          frame.appendChild(bubble);
+          nodes.push(bubble);
+        } else {};
+
+        vertexPos.push(bubblePos[i][l]);
+
+        console.log(vertexPos.length);
+
+      }
     }
 
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    //Vector ——————————————————————————————————————————————————
+
+
+
+    for (let i = 0; i < 4; i++) {
+      if (Math.random() < 0.5) {
+
+        let randomVertex = getRandomInt(0, vertexPos.length - 1);
+
+        const vector = figma.createVector();
+        vector.vectorNetwork = {
+          // The vertices of the triangle
+          vertices: [{
+              x: vertexPos[randomVertex].x + pillarW / 2,
+              y: vertexPos[randomVertex].y + pillarW / 2,
+              strokeCap: "ROUND"
+            },
+            {
+              x: vertexPos[randomVertex + 1].x + pillarW / 2,
+              y: vertexPos[randomVertex + 1].y + pillarW / 2,
+              strokeCap: "ROUND"
+            },
+          ],
+
+          segments: [{
+            start: 0,
+            tangentStart: {
+              x: 0,
+              y: 0
+            }, // optional
+            end: 1,
+            tangentEnd: {
+              x: 0,
+              y: 0
+            }, // optional
+          }],
+
+          regions: [{
+            windingRule: "NONZERO",
+            loops: [
+              [0]
+            ]
+          }],
+        }
+
+        vector.strokeWeight = pillarW;
+
+        vector.strokes = [{
+          type: 'SOLID',
+          color: {
+            r: Number(colorValVector[0]),
+            g: Number(colorValVector[1]),
+            b: Number(colorValVector[2])
+          }
+        }];
+
+
+        frame.appendChild(vector);
+        nodes.push(vector);
+
+
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
+
+      } else {};
+    }
+
   }
 
-  figma.closePlugin();
+  if (msg.type == 'clone') {
+    console.log("clone");
+    frame.clone();
+    frame.x = 100;
+    frame.name = "saved-Song"
+  }
+
+
 };
+
+
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -3,44 +3,47 @@ import logoPng from "./logo.png";
 import logoSvg from "./logo.svg?raw";
 import Logo from "./Logo";
 import "./App.css";
+import "../node_modules/figma-plugin-ds/dist/figma-plugin-ds.css"
+import { selectMenu, disclosure } from 'figma-plugin-ds';
+
+
+    selectMenu.init(); //initiates the select menu component
+    disclosure.init(); //initiates the disclosure component
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
 
-  const onCreate = () => {
-    const count = Number(inputRef.current?.value || 0);
-    parent.postMessage(
-      { pluginMessage: { type: "create-rectangles", count } },
-      "*"
-    );
-  };
+  // const onCreate = () => {
+  //   const count = Number(inputRef.current?.value || 0);
+  //   parent.postMessage(
+  //     { pluginMessage: { type: "create-rectangles", count } },
+  //     "*"
+  //   );
+  // };
 
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
-  };
+  // const onCancel = () => {
+  //   parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+  // };
 
-  return (
-    <main>
-      <header>
-        <img src={logoPng} />
-        &nbsp;
-        <img src={`data:image/svg+xml;utf8,${logoSvg}`} />
-        &nbsp;
-        <Logo />
-        <h2>Rectangle Creator</h2>
-      </header>
-      <section>
-        <input id="input" type="number" min="0" ref={inputRef} />
-        <label htmlFor="input">Rectangle Count</label>
-      </section>
-      <footer>
-        <button className="brand" onClick={onCreate}>
-          Create
-        </button>
-        <button onClick={onCancel}>Cancel</button>
-      </footer>
-    </main>
-  );
+  // return (
+  //   <main>
+
+  //     <section>
+  //       <input id="input" type="number" min="0" ref={inputRef} />
+  //       <label htmlFor="input">Rectangle Count</label>
+  //     </section>
+  //     <footer>
+  //       <button className="brand" onClick={onCreate}>
+  //         Create
+  //       </button>
+  //       <button onClick={onCancel}>Cancel</button>
+  //     </footer>
+  //   </main>
+  // );
+
 }
+
+
+
 
 export default App;
